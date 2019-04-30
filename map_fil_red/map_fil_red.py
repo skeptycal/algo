0854d154c0f4d86759a66ec4f0228c05cc352d17
skeptycal <skeptycal@gmail.com>
2019-04-29 21:13:12 -0500

    add map_fil_red

--- map_fil_red/map_fil_red.py
+++ map_fil_red/map_fil_red.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+""" map_fil_red.py """
+
 import math
+# import json
+# import secrets
+# import statistics
+# import sys
+import timeit
+from typing import Any, Dict, List, Tuple
+
+import numpy as np
+
+name = "map_fil_red.py"
+json_file = sys.path[0] + '/' + name + ".json"
+# https://www.youtube.com/watch?v=09JslnY7W_k
+reference = "https://www.youtube.com/watch?v=09JslnY7W_k"
+# pylint: disable=unused-variable,eval-used,missing-docstring
 
 
 def area(r: float) -> float:
