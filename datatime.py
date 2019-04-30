dac600d7f53d29688117f10d1438d1debc8ac22b
skeptycal <skeptycal@gmail.com>
2019-04-29 20:45:36 -0500

    initial commit

---
+++ datatime.py
@@ -0,0 +1,28 @@
+import re
+
+
+def convert_to_datetime(line: str):
+    match = re.search('\d{4}-\d{2}-\d{2}', line.strip('T')).group()
+    match += ' | ' + re.search('\d{2}:\d{2}:\d{2}', line).group()
+    return match
+
+
+def cut_out_datetime(line: str):
+    line = re.sub('ERROR ', "", line)
+    line = re.sub('T', " | ", line)
+    return line
+
+
+s = 'ERROR 2019-02-03T23:21:20'
+print('   Test string: ', s)
+print()
+print('Extract method: ', convert_to_datetime(s))
+print(' "Trim" method: ', cut_out_datetime(s))
+
+# OUTPUT:
+   Test string:  ERROR 2019-02-03T23:21:20
+
+Extract method:  2019-02-03 | 23:21:20
+ "Trim" method:  2019-02-03 | 23:21:20
+
+[Done] exited with code=0 in 0.05 seconds
---
+++ map_fil_red/map_fil_red.py
@@ -0,0 +1,6 @@
+import math
+
+
+def area(r: float) -> float:
+    """Area of a circle with radius 'r'."""
+    return math.pi * (r**2)
