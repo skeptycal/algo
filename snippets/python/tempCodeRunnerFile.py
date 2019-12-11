508fcc537480481d1877fde88997d4c130a64a6d
skeptycal <skeptycal@gmail.com>
2019-12-10 22:20:07 -0600

    Delete tempCodeRunnerFile.py

--- snippets/python/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-   # for i in range(2, int(math.sqrt(sieveSize)) + 1):
-    #     pointer = i * 2
-    #     while pointer < sieveSize:
-    #         sieve[pointer] = False
-    #         pointer += i
